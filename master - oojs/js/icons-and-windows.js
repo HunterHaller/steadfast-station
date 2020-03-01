5dbf6b0810e68fe4f540ded5128cdc5a3e5db4a4
Hunter Haller <huha3374@colorado.edu>
2020-03-01 00:53:15 -0700

    Added introWindow ID

--- master - oojs/js/icons-and-windows.js
+++ master - oojs/js/icons-and-windows.js
@@ -8,4 +8,4 @@ let playerIcon = new Icon(650, 150, "msc.png", "MSC-PLYR", "playerIcon");
 let playerWindow = new Window(160, 300, 138, 638, "MSC-PLYR", "<p>This is a new window!  Maybe one day it will play music or something...", "playerWindow");
 
 let introIcon = new Icon( 650, 250, "text.png", "INTRO.TXT", "introIcon");
-let introWindow = new Window(600, 300, 50, 1100, "INTRO.TXT", '<h1>Welcome, Technician!</h1><p>If this is your first time out among the stars, we\'re happy to have you! If not, you sure are here again and this file isn\'t for you!</p><p>Assuming you\'re near a BHW station, type <strong>"connect local"</strong> in your TRMNL to start interfacing with the station\'s AI.</p>');
+let introWindow = new Window(600, 300, 50, 1100, "INTRO.TXT", '<h1>Welcome, Technician!</h1><p>If this is your first time out among the stars, we\'re happy to have you! If not, you sure are here again and this file isn\'t for you!</p><p>Assuming you\'re near a BHW station, type <strong>"connect local"</strong> in your TRMNL to start interfacing with the station\'s AI.</p>', "introWindow");
